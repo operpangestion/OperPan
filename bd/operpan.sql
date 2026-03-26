c9278bcd857c878965b1d86693c41133e288b1b0
Eddier Antonio Paz Pardo <eddierpaz9@gmail.com>
2026-03-26 11:10:25 -0500

    OperPan 0.1.10: Base de Datos - Actulización Procesos, Triggers y Tablas

--- bd/operpan.sql
+++ bd/operpan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 26-03-2026 a las 15:15:11
+-- Tiempo de generación: 26-03-2026 a las 17:06:15
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -27,7 +27,7 @@ DELIMITER $$
 --
 CREATE DEFINER=`root`@`localhost` PROCEDURE `actualizar_estado_permiso` (IN `p_id_permiso` INT, IN `p_estado` VARCHAR(100))   BEGIN
   UPDATE permiso
-  SET estado = p_estado
+  SET estado_permiso = p_estado
   WHERE id_permiso = p_id_permiso;
 END$$
 
@@ -50,21 +50,52 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `insertar_memorando` (IN `p_id_emple
   );
 END$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `ver_empleados_memorandos` ()   BEGIN
-  SELECT CONCAT(e.nombre,' ',e.apellido) AS nombreCompleto,
-         m.id_memorando, m.descripcion
-  FROM empleado e
-  JOIN memorando m ON e.id_empleado = m.id_empleado;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `ver_empleado_horario` (IN `idEmpleado` INT)   BEGIN 
+	SELECT CONCAT(e.nombre, ' ',e.apellido) AS Nombre_Empleado, a.fecha_inicio, a.fecha_fin, h.jornada, a.fecha_descanso FROM empleado e
+    INNER JOIN asignacion_horario a ON a.id_empleado=e.id_empleado
+    INNER JOIN horario h ON h.id_horario=a.id_horario
+    WHERE idEmpleado=e.id_empleado;
 END$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `ver_empleados_permisos` ()   BEGIN
-  SELECT CONCAT(e.nombre,' ',e.apellido) AS nombreCompleto,
-         p.fecha_inicio, p.descripcion, p.estado
-  FROM permiso p
-  JOIN empleado e ON p.id_empleado = e.id_empleado;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `ver_empleado_tarea` (IN `idEmpleado` INT)   BEGIN 
+	SELECT CONCAT(e.nombre, ' ' , e.apellido ) AS Nombre_Empleado,t.fecha_inicio, t.fecha_fin, t.descripcion_tarea, t.estado_tarea  
+    FROM empleado e
+    INNER JOIN tarea t ON t.id_empleado = e.id_empleado
+	WHERE idEmpleado = e.id_empleado;
 END$$
 
 DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `asignacion_horario`
+--
+
+CREATE TABLE `asignacion_horario` (
+  `id_asignacion` int(11) NOT NULL,
+  `id_empleado` int(11) DEFAULT NULL,
+  `id_horario` int(11) DEFAULT NULL,
+  `fecha_inicio` date DEFAULT NULL,
+  `fecha_fin` date DEFAULT NULL,
+  `fecha_descanso` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `asignacion_horario`
+--
+
+INSERT INTO `asignacion_horario` (`id_asignacion`, `id_empleado`, `id_horario`, `fecha_inicio`, `fecha_fin`, `fecha_descanso`) VALUES
+(1, 1, 1, '2026-03-01', '2026-03-07', '2026-03-05'),
+(2, 2, 2, '2026-03-01', '2026-03-07', '2026-03-02'),
+(3, 3, 1, '2026-03-01', '2026-03-07', '2026-03-04'),
+(4, 4, 3, '2026-03-01', '2026-03-07', '2026-03-06'),
+(5, 5, 2, '2026-03-01', '2026-03-07', '2026-03-07'),
+(6, 1, 2, '2026-03-08', '2026-03-14', '2026-03-09'),
+(7, 2, 3, '2026-03-08', '2026-03-14', '2026-03-10'),
+(8, 3, 2, '2026-03-08', '2026-03-14', '2026-03-11'),
+(9, 4, 1, '2026-03-08', '2026-03-14', '2026-03-12'),
+(10, 5, 3, '2026-03-08', '2026-03-14', '2026-03-13');
 
 -- --------------------------------------------------------
 
@@ -106,26 +137,25 @@ CREATE TABLE `empleado` (
   `fecha_inicio_contrato` date DEFAULT NULL,
   `fecha_fin_contrato` date DEFAULT NULL,
   `estado` enum('Activo','Inactivo') DEFAULT NULL,
-  `id_horario` int(11) DEFAULT NULL,
-  `id_cargo` int(11) DEFAULT NULL
+  `id_cargo` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `empleado`
 --
 
-INSERT INTO `empleado` (`id_empleado`, `nombre`, `apellido`, `documento`, `telefono`, `direccion`, `correo`, `fecha_inicio_contrato`, `fecha_fin_contrato`, `estado`, `id_horario`, `id_cargo`) VALUES
-(1, 'Juan', 'Perez', 1010, 2147483647, 'Calle 1', 'juan@gmail.com', '2024-01-10', NULL, 'Activo', 1, 1),
-(2, 'Maria', 'Lopez', 1020, 300222222, 'Calle 2', 'maria@gmail.com', '2023-05-15', NULL, 'Activo', 2, 1),
-(3, 'Carlos', 'Gomez', 1030, 300333333, 'Calle 3', 'carlos@gmail.com', '2024-02-20', NULL, 'Activo', 3, 2),
-(4, 'Ana', 'Martinez', 1040, 300444444, 'Calle 4', 'ana@gmail.com', '2026-01-01', NULL, 'Activo', 5, 3),
-(5, 'Luis', 'Rodriguez', 1050, 300555555, 'Calle 5', 'luis@gmail.com', '2023-03-12', NULL, 'Activo', 7, 4),
-(6, 'Pedro', 'Ramirez', 1060, 300666666, 'Calle 6', 'pedro@gmail.com', '2021-07-01', NULL, 'Activo', 8, 4),
-(7, 'Andres', 'Lopez', 1080, 300888888, 'Calle 7', 'andres@gmail.com', '2025-01-01', NULL, 'Activo', 9, 5),
-(8, 'Sofia', 'Morales', 1100, 302123456, 'Calle 8', 'sofia@gmail.com', '2024-06-01', NULL, 'Activo', 6, 3),
-(9, 'Camila', 'Diaz', 1200, 303456789, 'Calle 9', 'camila@gmail.com', '2023-09-10', NULL, 'Activo', 4, 2),
-(10, 'David', 'Torres', 1300, 304567890, 'Calle 10', 'davidto@gmail.com', '2022-08-20', NULL, 'Activo', 2, 1),
-(11, 'Carlos', 'Ramírez', 2000, 310000000, 'Oficina principal', 'admin@operpan.com', '2026-01-01', NULL, 'Activo', 10, 6);
+INSERT INTO `empleado` (`id_empleado`, `nombre`, `apellido`, `documento`, `telefono`, `direccion`, `correo`, `fecha_inicio_contrato`, `fecha_fin_contrato`, `estado`, `id_cargo`) VALUES
+(1, 'Juan', 'Perez', 1010, 2147483647, 'Calle 1', 'juan@gmail.com', '2024-01-10', NULL, 'Activo', 1),
+(2, 'Maria', 'Lopez', 1020, 300222222, 'Calle 2', 'maria@gmail.com', '2023-05-15', NULL, 'Activo', 2),
+(3, 'Carlos', 'Gomez', 1030, 300333333, 'Calle 3', 'carlos@gmail.com', '2024-02-20', NULL, 'Activo', 3),
+(4, 'Ana', 'Martinez', 1040, 300444444, 'Calle 4', 'ana@gmail.com', '2026-01-01', NULL, 'Activo', 3),
+(5, 'Luis', 'Rodriguez', 1050, 300555555, 'Calle 5', 'luis@gmail.com', '2023-03-12', NULL, 'Activo', 4),
+(6, 'Pedro', 'Ramirez', 1060, 300666666, 'Calle 6', 'pedro@gmail.com', '2021-07-01', NULL, 'Activo', 4),
+(7, 'Andres', 'Lopez', 1080, 300888888, 'Calle 7', 'andres@gmail.com', '2025-01-01', NULL, 'Activo', 5),
+(8, 'Sofia', 'Morales', 1100, 302123456, 'Calle 8', 'sofia@gmail.com', '2024-06-01', NULL, 'Activo', 5),
+(9, 'Camila', 'Diaz', 1200, 303456789, 'Calle 9', 'camila@gmail.com', '2023-09-10', NULL, 'Activo', 2),
+(10, 'David', 'Torres', 1300, 304567890, 'Calle 10', 'davidto@gmail.com', '2022-08-20', NULL, 'Activo', 1),
+(11, 'Carlos', 'Ramírez', 2000, 310000000, 'Oficina principal', 'admin@operpan.com', '2026-01-01', NULL, 'Activo', 6);
 
 --
 -- Disparadores `empleado`
@@ -196,7 +226,18 @@ CREATE TABLE `empleado_auditoria` (
 INSERT INTO `empleado_auditoria` (`id_auditoria`, `id_empleado`, `nombre_old`, `apellido_old`, `documento_old`, `telefono_old`, `direccion_old`, `correo_old`, `nombre_new`, `apellido_new`, `documento_new`, `telefono_new`, `direccion_new`, `correo_new`, `fecha`, `usuario`, `accion`) VALUES
 (1, 12, 'Laura', 'Gómez', 1400, 305678901, 'Calle 12', 'laura.gomez@operpan.com', NULL, NULL, NULL, NULL, NULL, NULL, '2026-03-25 19:53:54', 'root@localhost', 'Eliminación de empleado'),
 (2, 10, 'David', 'Torres', 1300, 304567890, 'Calle 10', 'davidtorres@gmail.com', 'David', 'Torres', 1300, 304567890, 'Calle 10', 'davidto@gmail.com', '2026-03-25 19:56:27', 'root@localhost', 'Actualización de empleado'),
-(3, 1, 'Juan', 'Perez', 1010, 300111111, 'Calle 1', 'juan@gmail.com', 'Juan', 'Perez', 1010, 2147483647, 'Calle 1', 'juan@gmail.com', '2026-03-26 07:12:05', 'root@localhost', 'Actualización de empleado');
+(3, 1, 'Juan', 'Perez', 1010, 300111111, 'Calle 1', 'juan@gmail.com', 'Juan', 'Perez', 1010, 2147483647, 'Calle 1', 'juan@gmail.com', '2026-03-26 07:12:05', 'root@localhost', 'Actualización de empleado'),
+(4, 1, 'Juan', 'Perez', 1010, 2147483647, 'Calle 1', 'juan@gmail.com', 'Juan', 'Perez', 1010, 2147483647, 'Calle 1', 'juan@gmail.com', '2026-03-26 10:32:19', 'root@localhost', 'Actualización de empleado'),
+(5, 11, 'Carlos', 'Ramírez', 2000, 310000000, 'Oficina principal', 'admin@operpan.com', 'Carlos', 'Ramírez', 2000, 310000000, 'Oficina principal', 'admin@operpan.com', '2026-03-26 10:32:30', 'root@localhost', 'Actualización de empleado'),
+(6, 10, 'David', 'Torres', 1300, 304567890, 'Calle 10', 'davidto@gmail.com', 'David', 'Torres', 1300, 304567890, 'Calle 10', 'davidto@gmail.com', '2026-03-26 10:32:45', 'root@localhost', 'Actualización de empleado'),
+(7, 9, 'Camila', 'Diaz', 1200, 303456789, 'Calle 9', 'camila@gmail.com', 'Camila', 'Diaz', 1200, 303456789, 'Calle 9', 'camila@gmail.com', '2026-03-26 10:32:52', 'root@localhost', 'Actualización de empleado'),
+(8, 2, 'Maria', 'Lopez', 1020, 300222222, 'Calle 2', 'maria@gmail.com', 'Maria', 'Lopez', 1020, 300222222, 'Calle 2', 'maria@gmail.com', '2026-03-26 10:33:01', 'root@localhost', 'Actualización de empleado'),
+(9, 3, 'Carlos', 'Gomez', 1030, 300333333, 'Calle 3', 'carlos@gmail.com', 'Carlos', 'Gomez', 1030, 300333333, 'Calle 3', 'carlos@gmail.com', '2026-03-26 10:33:14', 'root@localhost', 'Actualización de empleado'),
+(10, 4, 'Ana', 'Martinez', 1040, 300444444, 'Calle 4', 'ana@gmail.com', 'Ana', 'Martinez', 1040, 300444444, 'Calle 4', 'ana@gmail.com', '2026-03-26 10:33:23', 'root@localhost', 'Actualización de empleado'),
+(11, 5, 'Luis', 'Rodriguez', 1050, 300555555, 'Calle 5', 'luis@gmail.com', 'Luis', 'Rodriguez', 1050, 300555555, 'Calle 5', 'luis@gmail.com', '2026-03-26 10:33:33', 'root@localhost', 'Actualización de empleado'),
+(12, 6, 'Pedro', 'Ramirez', 1060, 300666666, 'Calle 6', 'pedro@gmail.com', 'Pedro', 'Ramirez', 1060, 300666666, 'Calle 6', 'pedro@gmail.com', '2026-03-26 10:33:39', 'root@localhost', 'Actualización de empleado'),
+(13, 7, 'Andres', 'Lopez', 1080, 300888888, 'Calle 7', 'andres@gmail.com', 'Andres', 'Lopez', 1080, 300888888, 'Calle 7', 'andres@gmail.com', '2026-03-26 10:33:45', 'root@localhost', 'Actualización de empleado'),
+(14, 8, 'Sofia', 'Morales', 1100, 302123456, 'Calle 8', 'sofia@gmail.com', 'Sofia', 'Morales', 1100, 302123456, 'Calle 8', 'sofia@gmail.com', '2026-03-26 10:33:51', 'root@localhost', 'Actualización de empleado');
 
 -- --------------------------------------------------------
 
@@ -207,25 +248,24 @@ INSERT INTO `empleado_auditoria` (`id_auditoria`, `id_empleado`, `nombre_old`, `
 CREATE TABLE `horario` (
   `id_horario` int(11) NOT NULL,
   `jornada` varchar(100) DEFAULT NULL,
-  `descripcion` varchar(255) DEFAULT NULL,
-  `id_cargo` int(11) DEFAULT NULL
+  `descripcion` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `horario`
 --
 
-INSERT INTO `horario` (`id_horario`, `jornada`, `descripcion`, `id_cargo`) VALUES
-(1, '4:00 AM - 2:00 PM', 'Turno mañana meseros', 1),
-(2, '1:30 PM - 11:00 PM', 'Turno tarde meseros', 1),
-(3, '4:00 AM - 2:00 PM', 'Turno mañana cajero', 2),
-(4, '1:30 PM - 11:00 PM', 'Turno tarde cajero', 2),
-(5, '4:00 AM - 2:00 PM', 'Preparación desayuno y almuerzo', 3),
-(6, '7:00 AM - 5:00 PM', 'Producción continua cocina', 3),
-(7, '5:00 AM - 2:00 PM', 'Producción de pan temprano', 4),
-(8, '1:00 PM - 10:00 PM', 'Reposición y producción tarde', 4),
-(9, '7:00 AM - 5:00 PM', 'Producción y decoración pastelería', 5),
-(10, '8:00 AM - 6:00 PM', 'Horario administrativo', 6);
+INSERT INTO `horario` (`id_horario`, `jornada`, `descripcion`) VALUES
+(1, '4:00 AM - 2:00 PM', 'Turno mañana meseros'),
+(2, '1:30 PM - 11:00 PM', 'Turno tarde meseros'),
+(3, '4:00 AM - 2:00 PM', 'Turno mañana cajero'),
+(4, '1:30 PM - 11:00 PM', 'Turno tarde cajero'),
+(5, '4:00 AM - 2:00 PM', 'Preparación desayuno y almuerzo'),
+(6, '7:00 AM - 5:00 PM', 'Producción continua cocina'),
+(7, '5:00 AM - 2:00 PM', 'Producción de pan temprano'),
+(8, '1:00 PM - 10:00 PM', 'Reposición y producción tarde'),
+(9, '7:00 AM - 5:00 PM', 'Producción y decoración pastelería'),
+(10, '8:00 AM - 6:00 PM', 'Horario administrativo');
 
 -- --------------------------------------------------------
 
@@ -307,7 +347,7 @@ CREATE TABLE `permiso` (
 --
 
 INSERT INTO `permiso` (`id_permiso`, `id_empleado`, `fecha_subida`, `fecha_inicio`, `fecha_fin`, `descripcion`, `estado_permiso`) VALUES
-(1, 1, '2026-03-01', '2026-03-05', '2026-03-06', 'Cita médica', 'Aprobado'),
+(1, 1, '2026-03-01', '2026-03-05', '2026-03-06', 'Cita médica', 'No Aprobado'),
 (2, 2, '2026-03-02', '2026-03-07', '2026-03-08', 'Asunto personal', 'No Aprobado'),
 (3, 3, '2026-03-03', '2026-03-10', '2026-03-11', 'Calamidad familiar', 'Aprobado'),
 (4, 4, '2026-03-04', '2026-03-12', '2026-03-13', 'Estudio', 'No Aprobado'),
@@ -436,8 +476,11 @@ INSERT INTO `tipo_tarea` (`id_tipo_tarea`, `nombre_tipo_tarea`) VALUES
 --
 CREATE TABLE `vista_empleado_horario` (
 `id_empleado` int(11)
-,`nombre_completo` varchar(201)
+,`Nombre_Empleado` varchar(201)
+,`fecha_inicio` date
+,`fecha_fin` date
 ,`jornada` varchar(100)
+,`fecha_descanso` date
 );
 
 -- --------------------------------------------------------
@@ -474,7 +517,7 @@ CREATE TABLE `vista_memorandos_por_empleado` (
 --
 DROP TABLE IF EXISTS `vista_empleado_horario`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vista_empleado_horario`  AS SELECT `e`.`id_empleado` AS `id_empleado`, concat(`e`.`nombre`,' ',`e`.`apellido`) AS `nombre_completo`, `h`.`jornada` AS `jornada` FROM (`empleado` `e` join `horario` `h` on(`e`.`id_horario` = `h`.`id_horario`)) ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vista_empleado_horario`  AS SELECT `e`.`id_empleado` AS `id_empleado`, concat(`e`.`nombre`,' ',`e`.`apellido`) AS `Nombre_Empleado`, `a`.`fecha_inicio` AS `fecha_inicio`, `a`.`fecha_fin` AS `fecha_fin`, `h`.`jornada` AS `jornada`, `a`.`fecha_descanso` AS `fecha_descanso` FROM ((`empleado` `e` join `asignacion_horario` `a` on(`a`.`id_empleado` = `e`.`id_empleado`)) join `horario` `h` on(`a`.`id_horario` = `h`.`id_horario`)) ;
 
 -- --------------------------------------------------------
 
@@ -499,6 +542,14 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 --
 
 --
+-- Indices de la tabla `asignacion_horario`
+--
+ALTER TABLE `asignacion_horario`
+  ADD PRIMARY KEY (`id_asignacion`),
+  ADD KEY `id_empleado` (`id_empleado`),
+  ADD KEY `id_horario` (`id_horario`);
+
+--
 -- Indices de la tabla `cargo`
 --
 ALTER TABLE `cargo`
@@ -509,8 +560,7 @@ ALTER TABLE `cargo`
 --
 ALTER TABLE `empleado`
   ADD PRIMARY KEY (`id_empleado`),
-  ADD KEY `id_horario` (`id_horario`),
-  ADD KEY `id_cargo` (`id_cargo`);
+  ADD KEY `fk_empleado_cargo` (`id_cargo`);
 
 --
 -- Indices de la tabla `empleado_auditoria`
@@ -522,8 +572,7 @@ ALTER TABLE `empleado_auditoria`
 -- Indices de la tabla `horario`
 --
 ALTER TABLE `horario`
-  ADD PRIMARY KEY (`id_horario`),
-  ADD KEY `id_cargo` (`id_cargo`);
+  ADD PRIMARY KEY (`id_horario`);
 
 --
 -- Indices de la tabla `incapacidad`
@@ -571,6 +620,12 @@ ALTER TABLE `tipo_tarea`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `asignacion_horario`
+--
+ALTER TABLE `asignacion_horario`
+  MODIFY `id_asignacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
 -- AUTO_INCREMENT de la tabla `empleado`
 --
 ALTER TABLE `empleado`
@@ -580,7 +635,7 @@ ALTER TABLE `empleado`
 -- AUTO_INCREMENT de la tabla `empleado_auditoria`
 --
 ALTER TABLE `empleado_auditoria`
-  MODIFY `id_auditoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_auditoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT de la tabla `incapacidad`
@@ -623,17 +678,17 @@ ALTER TABLE `tipo_tarea`
 --
 
 --
+-- Filtros para la tabla `asignacion_horario`
+--
+ALTER TABLE `asignacion_horario`
+  ADD CONSTRAINT `asignacion_horario_ibfk_1` FOREIGN KEY (`id_empleado`) REFERENCES `empleado` (`id_empleado`),
+  ADD CONSTRAINT `asignacion_horario_ibfk_2` FOREIGN KEY (`id_horario`) REFERENCES `horario` (`id_horario`);
+
+--
 -- Filtros para la tabla `empleado`
 --
 ALTER TABLE `empleado`
-  ADD CONSTRAINT `empleado_ibfk_1` FOREIGN KEY (`id_horario`) REFERENCES `horario` (`id_horario`),
-  ADD CONSTRAINT `empleado_ibfk_2` FOREIGN KEY (`id_cargo`) REFERENCES `cargo` (`id_cargo`);
-
---
--- Filtros para la tabla `horario`
---
-ALTER TABLE `horario`
-  ADD CONSTRAINT `horario_ibfk_1` FOREIGN KEY (`id_cargo`) REFERENCES `cargo` (`id_cargo`);
+  ADD CONSTRAINT `fk_empleado_cargo` FOREIGN KEY (`id_cargo`) REFERENCES `cargo` (`id_cargo`);
 
 --
 -- Filtros para la tabla `incapacidad`
@@ -661,6 +716,9 @@ ALTER TABLE `tarea`
   ADD CONSTRAINT `tarea_ibfk_2` FOREIGN KEY (`id_empleado`) REFERENCES `empleado` (`id_empleado`);
 COMMIT;
 
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
